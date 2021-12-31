9eec434971a80100a9238873bce641e5c797e4f8
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-31 14:35:00 +0100

    Feature 🏋: Sorting linked list.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedListCollection";
 
 const numberCollections = new NumbersCollection([82, 4, 5, 0, -6, 10, 17, 80]);
 const sortNumbers = new Sorter(numberCollections);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection("Xasfbfafwaewr");
 const sortStrings = new Sorter(charactersCollection);
 sortStrings.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(10);
+linkedList.add(-20);
+linkedList.add(5);
+
+const sortLinkedList = new Sorter(linkedList);
+sortLinkedList.sort();
+linkedList.print();
