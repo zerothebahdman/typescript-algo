43dd2f4581ef52075aa385b3a23bc4cbd26dbf2c
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-31 12:33:44 +0100

    Chore ⚙: Using type guard to check if the type of collection given.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   // Implementing sorting with bubble sort algorithm
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHandSide = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHandSide;
+        // This is going to work if we have and array of numbers numbers[]
+        // ~~~~ Logic to comapre and swap numbers in an array in assending order ~~~~
+
+        // A type guard is some expression that performs a runtime check that guarantees the type in some scope.
+        if (this.collection instanceof Array) {
+          // using type guard to check if the given collection is an array of numbers (numbers[]) || a string.
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHandSide = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHandSide;
+          }
+        }
+
+        // This going to work if we have a string
+        // ~~~~~ Logic to compare and swap characters in a string ~~~~~
+        if (typeof this.collection === "string") {
         }
       }
     }
